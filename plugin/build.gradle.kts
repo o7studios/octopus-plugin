14a48d5e6606bfa65e547abb82fbe1d950241176
Julian Siebert <mail@julian-siebert.de>
2025-09-23 19:46:52 +0000

    feat: Updated Remora settings

--- plugin/build.gradle.kts
+++ plugin/build.gradle.kts
@@ -1,3 +1,18 @@
+import studio.o7.remora.plugin.ApiVersion
+import studio.o7.remora.plugin.Load
+
 dependencies {
     implementation(project(":api"))
+}
+
+information {
+    artifactId = "octopus"
+    description = "Octopus paper plugin"
+}
+
+plugin {
+    enabled = true
+    main = "studio.o7.octopus.plugin.OctopusPlugin"
+    apiVersion = ApiVersion.PAPER_1_21_8
+    load.set(Load.POST_WORLD)
 }
