42761bbf838d991068db5c8b4c617e9fef5118fa
Julian Siebert <mail@julian-siebert.de>
2025-09-23 19:51:29 +0000

    fix: api paper api

--- api/build.gradle.kts
+++ api/build.gradle.kts
@@ -1,5 +1,10 @@
+repositories {
+    maven("https://repo.papermc.io/repository/maven-public/")
+}
+
 dependencies {
     api("studio.o7:octopus-sdk:0.3.3")
+    compileOnly("io.papermc.paper:paper-api:1.21.8-R0.1-SNAPSHOT")
 }
 
 information {
