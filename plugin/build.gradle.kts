c3ab3289eed317eb3ef930a54262e7f330373bfe
raphael-goetz <raphael.goetz.7@gmail.com>
2026-01-27 10:56:55 +0100

    dependencies: updated remora, added grpc dependencies and setup shadowJar

--- plugin/build.gradle.kts
+++ plugin/build.gradle.kts
@@ -1,13 +1,23 @@
+import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
 import studio.o7.remora.plugin.ApiVersion
 import studio.o7.remora.plugin.Load
 
+plugins {
+    id("xyz.jpenilla.run-paper") version "3.0.2"
+}
+
 dependencies {
     implementation(project(":api"))
+
+    implementation("io.grpc:grpc-okhttp:1.78.0")
+    implementation("io.grpc:grpc-protobuf:1.78.0")
+    implementation("io.grpc:grpc-stub:1.78.0")
 }
 
 information {
     artifactId = "octopus"
-    description = "Octopus paper plugin"
+    description = "Octopus-API paper plugin implementation"
+    url = "https://github.com/o7studios/octopus-plugin"
 }
 
 plugin {
@@ -15,4 +25,20 @@ plugin {
     main = "studio.o7.octopus.plugin.OctopusPlugin"
     apiVersion = ApiVersion.PAPER_1_21_8
     load.set(Load.POST_WORLD)
+}
+
+tasks.named<ShadowJar>("shadowJar") {
+    duplicatesStrategy = DuplicatesStrategy.EXCLUDE
+    mergeServiceFiles()
+
+    filesMatching("META-INF/services/**") {
+        duplicatesStrategy = DuplicatesStrategy.INCLUDE
+    }
+}
+
+tasks {
+    runServer {
+        minecraftVersion("1.21.11")
+        args("--port", "25565")
+    }
 }
